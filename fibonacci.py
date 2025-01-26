9a588c588fa5cd4631e0adb2dc53d4ac39c9159b
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-01-26 19:28:45 -0300

    :white_check_mark: Add test

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,18 @@
 def fibonacci(num):
     res = [0, 1]
     count = 1
+
     while (num > count):
         prevPos = count - 1
         nextPos = count
         nextVal = res[prevPos] + res[nextPos]
         res.append(nextVal)
         count = count + 1
+
     return res
 
 if __name__ == '__main__':
-    print(fibonacci(21))
+    result = fibonacci(5)
+    expect = [0, 1, 1, 2, 3, 5]
+    success = result == expect
+    print(success)
