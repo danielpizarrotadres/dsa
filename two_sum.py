3ce90291c8a28b06f4ff340b38772b2768c9bacd
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-01-26 17:34:55 -0300

    :recycle: Add snake_case

--- two_sum.py
+++ two_sum.py
@@ -1,5 +1,5 @@
 class Solution:
-  def twoSum(self, nums, target):
+  def two_sum(self, nums, target):
     result = [] # 1
     for i, num in enumerate(nums):
       for j, pair in enumerate(nums): # n
@@ -16,4 +16,4 @@ class Solution:
 
 if __name__ == "__main__":
   sol = Solution()
-  print(sol.twoSum([2, 7, 11, 15], 9))
+  print(sol.two_sum([2, 7, 11, 15], 9))
