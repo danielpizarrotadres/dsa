fa94934380b9041e46da9dd9e3ef7a28da33ae78
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-01-26 19:36:32 -0300

    :bulb: Add time complexity

--- fibonacci.py
+++ fibonacci.py
@@ -1,15 +1,19 @@
 def fibonacci(num):
-    res = [0, 1]
-    count = 1
+    res = [0, 1] # 1
+    count = 1 # 1
 
-    while (num > count):
-        prevPos = count - 1
-        nextPos = count
-        nextVal = res[prevPos] + res[nextPos]
-        res.append(nextVal)
-        count = count + 1
+    while (num > count): # n
+        prevPos = count - 1 # 1
+        nextPos = count # 1
+        nextVal = res[prevPos] + res[nextPos] # 1
+        res.append(nextVal) # 1
+        count = count + 1 # 1
 
-    return res
+    return res # 1
+
+# T = 1 + 1 + 1 + 1 + 1 + 1 + 1 + n
+# T = 7 + n
+# T = n
 
 if __name__ == '__main__':
     result = fibonacci(5)
