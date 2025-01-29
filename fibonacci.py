5a8e736ece89d2f958a50acac35479345a30762c
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-01-29 17:14:41 -0300

    :adhesive_bandage:

--- fibonacci.py
+++ fibonacci.py
@@ -1,21 +1,25 @@
 def fibonacci(num):
     res = [0, 1] # 1
     count = 1 # 1
-
-    while (num > count): # n
+    if num == 0:
+        return [0]
+    if num == 1:
+        return res
+    if num == 2:
+        return res 
+    while (num - 1 > count): # n
         prevPos = count - 1 # 1
         nextPos = count # 1
         nextVal = res[prevPos] + res[nextPos] # 1
         res.append(nextVal) # 1
         count = count + 1 # 1
-
     return res # 1
 
 # T = 1 + 1 + 1 + 1 + 1 + 1 + 1 + n
 # T = 7 + n
 # T = n
 
-def recursive_fibonacci(f, aux=0, prev=0, next=1, accum=""):
+def recursive_fibonacci(f, aux=1, prev=0, next=1, accum=""):
     newNext = prev + next
     newPrev = next
     aux = aux + 1
@@ -25,5 +29,5 @@ def recursive_fibonacci(f, aux=0, prev=0, next=1, accum=""):
     return "0 1"  + str(newAccum)
 
 if __name__ == '__main__':
-    print(fibonacci(7))
-    print(recursive_fibonacci(7))
+    print(fibonacci(5))
+    print(recursive_fibonacci(1))
