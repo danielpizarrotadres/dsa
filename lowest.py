cadb8e74e17121fea1bbbfacc49033d3fb6b2a15
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-01-29 21:54:18 -0300

    :wrench: Add base for lowest

--- lowest.py
+++ lowest.py
@@ -1,1 +1,17 @@
+def lowest(values):
+    if len(values) == 0:
+        return 0
+    if len(values) == 1:
+        return values[0]
+    lowest = 0
+    aux = 0
+    while len(values) > aux:
+        current = values[aux - 1]
+        next = values[aux]
+        if current < next:
+            lowest = current
+        aux = aux + 1
+    return lowest
 
+if __name__ == '__main__':
+    print(lowest([11, 12, 15, 1]))
