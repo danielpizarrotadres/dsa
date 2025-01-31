0f1256dc5c5bd24dc8f471540e244ed4a88b5b42
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-01-30 21:28:59 -0300

    :construction: Implement using pop to go removing the lowest

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,5 +1,13 @@
-def bubble_sort():
-    return 1
+from lowest import lowest_index
+from highest import highest
 
+def bubble_sort(nums):
+    numbers = nums
+    temp = []
+    aux = 0
+    while len(nums) > 0:
+        nums.pop(len(nums)-1)
+    return nums
+ 
 if __name__ == '__main__':
-    print(bubble_sort())
+    print(bubble_sort([7, 3, 5, 8, 6]))
