35eec35ef5b9217de20fb3146c857ba3ad5d9266
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-01-30 21:59:03 -0300

    :truck: Move lowest_index function to lowest.py file

--- lowest.py
+++ lowest.py
@@ -15,5 +15,23 @@ def lowest(values):
         aux = aux + 1
     return lowest
 
+def lowest_index(values):
+    if len(values) == 0:
+        return 0
+    if len(values) == 1:
+        return values[0]
+    lowest_index = 0
+    aux = 1
+    while len(values) > aux:
+        current_index = aux - 1
+        next_inext = aux
+        if current_index < next_index:
+           lowest_index = current_index
+        else:
+           lowest_index = next_index
+        aux = aux + 1
+    return lowest_index
+
 if __name__ == '__main__':
     print(lowest([11, 12, 15, 1]))
+    print(lowest_index([11, 12, 15, 1]))
