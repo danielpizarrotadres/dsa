ddbad6cdea1a2de3d4d8025da671db82f847c3f3
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-02-06 23:29:31 -0300

    :construction: WIP bubble_sort algorithm

--- bubble_sort.py
+++ bubble_sort.py
@@ -6,8 +6,12 @@ def bubble_sort(nums):
     temp = []
     aux = 0
     while len(nums) > 0:
-        nums.pop(len(nums)-1)
-    return nums
+        print("Printing current nums: ", nums)
+        lowest_idx = lowest_index(nums)
+        print("Printing lowest_idx: ", lowest_idx)
+        temp.insert(aux, nums[lowest_idx])
+        nums.pop(lowest_idx)
+    return temp
  
 if __name__ == '__main__':
     print(bubble_sort([7, 3, 5, 8, 6]))
