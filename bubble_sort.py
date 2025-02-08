4f7e20a357f1620f58ed3fcd8ac70d8c1acf5cd3
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-02-08 15:51:50 -0300

    :recycle: New bubble_sort implementation avoid using lowest_index and higest functions

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,16 +1,26 @@
 from lowest import lowest_index
 from highest import highest
 
+# def bubble_sort(nums):
+#     numbers = nums
+#     temp = []
+#     aux = 0
+#     while len(nums) > 0:
+#         lowest_idx = lowest_index(nums)
+#         temp.insert(aux, nums[lowest_idx])
+#         nums.pop(lowest_idx)
+#         aux = aux + 1
+#     return temp
+
 def bubble_sort(nums):
-    numbers = nums
-    temp = []
-    aux = 0
-    while len(nums) > 0:
-        lowest_idx = lowest_index(nums)
-        temp.insert(aux, nums[lowest_idx])
-        nums.pop(lowest_idx)
-        aux = aux + 1
-    return temp
- 
+    for i, num in enumerate(nums):
+        for j in range(len(nums) - 1):
+            aux = nums[j]
+            if nums[j] > nums[j+1]:
+                aux = nums[j]
+                if nums[j] > nums[j+1]:
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+    return nums
+    
 if __name__ == '__main__':
     print(bubble_sort([7, 3, 5, 8, 6]))
