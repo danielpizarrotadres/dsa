d6b6af1fc7b85ff2c1d0e29f3cf918d96c6b9440
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-02-17 21:28:03 -0300

    :construction: WIP selection_sort algorithm

--- selection_sort.py
+++ selection_sort.py
@@ -1,1 +1,9 @@
-# TODO: Implement
+def selection_sort(nums):
+    lowest = 0
+    temp = []
+    for i, num in enumerate(nums):
+        print(i)
+    return nums
+
+if __name__ == '__main__':
+    print(selection_sort([3, 5, 2, 8, 1, 100]))
