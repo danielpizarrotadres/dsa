43f852f972d141d32678559d63c1defee2c9c540
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-02-18 21:23:43 -0300

    :construction: Add lowest_value and lowest_pos vars

--- selection_sort.py
+++ selection_sort.py
@@ -1,5 +1,6 @@
 def selection_sort(nums):
-    lowest = 0
+    lowest_value = 0
+    lowest_pos = 0
     temp = []
     for i, num in enumerate(nums):
         print(f'Current: {i}')
@@ -11,21 +12,26 @@ def selection_sort(nums):
 
             if j == 0:
                 if nums[j] > nums[j+1]:
-                    lowest = nums[j+1]
+                    lowest_value = nums[j+1]
+                    lowest_pos = j+1
                 if nums[j+1] > nums[j]:
-                    lowest = nums[j]
+                    lowest_value = nums[j]
+                    lowest_pos = j
 
             if j+1 == len(nums):
                 print(f'Current j: {j} is equal to len(nums): {len(nums)}')
                 continue
 
             if j > 0:
-                if nums[j] > nums[j+1] and lowest >= nums[j+1]:
-                    lowest = nums[j+1]
-                if nums[j+1] > nums[j] and lowest >= nums[j]:
-                    lowest = nums[j]
+                if nums[j] > nums[j+1] and lowest_value >= nums[j+1]:
+                    lowest_value = nums[j+1]
+                    lowest_pos = j+1
+                if nums[j+1] > nums[j] and lowest_value >= nums[j]:
+                    lowest_value = nums[j]
+                    lowest_pos = j
 
-        print(f'Lowest: {lowest}')
+        print(f'lowest_value: {lowest_value}')
+        print(f'lowest_pos: {lowest_pos}')
     return nums
 
 if __name__ == '__main__':
