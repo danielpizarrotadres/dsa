d5dee53232573dcc8840b4d9d003da4da68c5877
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-02-25 06:24:37 -0300

    :sparkles: Add selection_sort

--- selection_sort.py
+++ selection_sort.py
@@ -1,40 +1,25 @@
 def selection_sort(nums):
-    lowest_value = 0
+    sorted_nums = []
+    temp = nums
+    aux = 0
+    lowest_val = nums[0]
     lowest_pos = 0
-    temp = []
-    for i, num in enumerate(nums):
-        print(f'Current: {i}')
+    while len(nums) > aux:
+        temp = temp[0:len(temp)]
+        for index in range(len(temp)-1):
+            if temp[index] < temp[index+1] and temp[index] <= lowest_val:
+                lowest_val = temp[index]
+                lowest_pos = index
+        sorted_nums.append(lowest_val)
+        temp.pop(lowest_pos)
 
-        # Find lowest
-        for j, item in enumerate(nums):
-
-            print(f'Current j: {j}')
-
-            if j == 0:
-                if nums[j] > nums[j+1]:
-                    lowest_value = nums[j+1]
-                    lowest_pos = j+1
-                if nums[j+1] > nums[j]:
-                    lowest_value = nums[j]
-                    lowest_pos = j
-
-            if j+1 == len(nums):
-                print(f'Current j: {j} is equal to len(nums): {len(nums)}')
-                continue
-
-            if j > 0:
-                if nums[j] > nums[j+1] and lowest_value >= nums[j+1]:
-                    lowest_value = nums[j+1]
-                    lowest_pos = j+1
-                if nums[j+1] > nums[j] and lowest_value >= nums[j]:
-                    lowest_value = nums[j]
-                    lowest_pos = j
-
-        print(f'lowest_value: {lowest_value}')
-        print(f'lowest_pos: {lowest_pos}')
-    return nums
+        if len(temp) > 0:
+            lowest_val = temp[0]
+            lowest_pos = 0
+        
+        aux += 1
+    return sorted_nums
 
 if __name__ == '__main__':
     nums = [2, 4, 100, 1, 5, 101]
-    # nums.pop(0)
     print(selection_sort(nums))
