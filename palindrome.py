4e00887db9db08e2217914d7b1e58cd7f4f8baf4
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-03-04 05:49:15 -0300

    :sparkles: Add palindrome implementation

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,16 @@
 def is_palindrome(word):
-    return True
+    word_backward = ''
+    i = 0
+    while len(word) > i:
+        word_backward = word_backward + word[(len(word)-i)-1]
+        i+=1
+    if word == word_backward:
+        return True
+    return False
 
 if __name__ == '__main__':
     print(is_palindrome('ana'))
+    print(is_palindrome('madam'))
+    print(is_palindrome('hello'))
+    print(is_palindrome('a'))
+    print(is_palindrome(''))
