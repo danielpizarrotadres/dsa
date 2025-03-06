71f0d32fefaa67408457b636496fbac01511f044
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-03-06 06:52:42 -0300

    :sparkles: Implement loop_reverse

--- loop_reverse.py
+++ loop_reverse.py
@@ -1,4 +1,7 @@
 def loop_reverse(nums):
+    for index in range(len(nums)):
+        last_value = nums[len(nums)-1 - index]
+        print(f'Current index: {index}: last_value: {last_value}')
     return nums
 
 if __name__ == '__main__':
