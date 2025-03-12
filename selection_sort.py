aebd3579089b63e06144fe732ef0ef9ca1f7a4b8
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-03-12 06:22:13 -0300

    :sparkles: Implement selection_sort using swaping list

--- selection_sort.py
+++ selection_sort.py
@@ -16,14 +16,13 @@ def selection_sort(nums):
     return nums
 
 def selection_sort_swaping(nums):
-    # TODO: Implement swap: https://www.w3schools.com/dsa/dsa_algo_selectionsort.php
     i = 0
     while len(nums) > i:
         lowest_index = i
-        lowest_value = nums[i]
         for j in range(i, len(nums)-1):
-            # TODO: Add comparison between current j and current j+1
-            pass
+            if nums[j] > nums[j+1] and nums[j+1] <= nums[lowest_index]:
+                lowest_index = j+1
+        nums[i], nums[lowest_index] = nums[lowest_index], nums[i]
         i += 1
     return nums
 
@@ -36,4 +35,9 @@ if __name__ == '__main__':
     print(f'[1, 2, 3, 4] -> {selection_sort([1, 2, 3, 4])}')
     print(f'[5, 1, 4, 2, 8] -> {selection_sort([5, 1, 4, 2, 8])}')
     print(f'[10, -1, 2, 5, 0] -> {selection_sort([10, -1, 2, 5, 0])}')
+    print(f'[5, 4, 3]/{selection_sort_swaping([5, 4, 3])}')
+    print(f'[5, 3, 4]/{selection_sort_swaping([5, 3, 4])}')
+    print(f'[-5, -3, 4]/{selection_sort_swaping([-5, -3, 4])}')
+    print(f'[3, 1, 2]/{selection_sort_swaping([3, 1, 2])}')
+    print(f'[5, 1, 4, 2, 8] -> {selection_sort_swaping([5, 1, 4, 2, 8])}')
     print(f'[10, -1, 2, 5, 0] -> {selection_sort_swaping([10, -1, 2, 5, 0])}')
