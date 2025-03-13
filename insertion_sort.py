439b9c1a745ac683c5670d7236381040f9d01961
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-03-13 06:58:43 -0300

    :construction: WIP insertion_sort()

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,15 +1,10 @@
 def insertion_sort(nums):
     i = 0
-    while len(nums)-1 >= i:
-        temp = nums[0:i+1]
-        last_value = temp[len(temp)-1]
-        print(f'\nCurrent temp: {temp}, printing last value: {last_value}')
-        for index in range(len(temp)):
-            print(f'Current index: {index}')
+    while len(nums) > i:
+        temp = nums[:i+1]
+        print(f'Current index: {i}, temp: {temp}')
         i += 1
     return nums
 
 if __name__ == '__main__':
-    # print(insertion_sort([5, 4, 3, 2, 1])) # Fails
-    # print(insertion_sort([3, -1, 2, -3, 1])) # Fails
     print(insertion_sort([5, 4, 3]))
