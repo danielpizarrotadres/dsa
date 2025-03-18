66653df2c48b24956e90bc07eeac198ce18053ab
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-03-18 06:35:11 -0300

    :construction: WIP insertion_sort()

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,6 +1,11 @@
 def insertion_sort(nums):
     for i in range(len(nums)-1):
-        next = nums[i+1]
+        next_value = nums[i+1]
+        temp = nums[:i+1]
+        for j in range(len(temp)):
+            last_value = temp[len(temp)-1 - j]
+            if last_value > next_value:
+                nums[len(temp)-1 - j], nums[i+1] = next_value, last_value    
     return nums
 
 if __name__ == '__main__':
