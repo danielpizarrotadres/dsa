7a9cd3f322bd0cc30a5ed5f4fa897dac7b27e698
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-03-22 18:33:48 -0300

    :bulb: Add reminders

--- quick_sort.py
+++ quick_sort.py
@@ -1,1 +1,9 @@
 # TODO: Implement quick_sort()
+
+# 1- Create a variable `pivot`
+# 2- Assign a value to variable `pivot` It should be the last number from list of numbers
+# 3- Loop over the list of numbers
+# 4- For each number, validate:
+# 4.1- If current number is greater than `pivot` and 
+# 4.2- If curret number is greater than next number
+# 5- If both conditions apply, swap current number with next number
