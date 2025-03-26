e71e641197c6c8f339444bc06501568f2ae52983
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2025-03-26 06:32:21 -0300

    :construction: WIP quick_sort()

--- quick_sort.py
+++ quick_sort.py
@@ -1,18 +1,11 @@
-# TODO: Implement quick_sort()
-
-# 1- Create a variable `pivot`
-# 2- Assign a value to variable `pivot` It should be the last number from list of numbers
-# 3- Loop over the list of numbers
-# 4- For each number, validate:
-# 4.1- If current number is greater than `pivot` and 
-# 4.2- If curret number is greater than next number
-# 5- Swap doesn't work, I should use the `remove` and `insert` approach
-
 def quick_sort(nums):
-    for index in range(1, len(nums)-1):
+    pivot = nums[len(nums)-1]
+    limit = len(nums)-1
+    for index in range(limit):
         current = nums[index]
-        previous = nums[index-1]
-        print(f'Printing index: {index} Printing current: {current} Printing previous {previous}')
+        if current >= pivot:
+            nums.pop(index)
+            nums.append(current)
     return nums
 
 if __name__ == '__main__':
